a76db26f8dbb5de4ae8cd4ed35b52c7e445e1c12
connor sanderford <connorsanderford@gmail.com>
2019-12-08 21:33:36 -0700

    add users through interface

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,24 +1,52 @@
+from decouple import config
 import uuid
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User, Tweet
+from .twitter import insert_user
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.sqlite'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['ENV'] = config('ENV')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
     def index():
-        rand_name = str(uuid.uuid4())
-        rand_u = User(name=rand_name)
-        DB.session.add(rand_u)
-        DB.session.commit()
-        return 'index page.'
+        #rand_name = str(uuid.uuid4())
+        #rand_u = User(name=rand_name)
+        #DB.session.add(rand_u)
+        #DB.session.commit()
+        users = User.query.all()
+        return render_template('base.html', title='index', users=users)
 
     @app.route('/hello')
     def hello():
         return render_template('base.html', title='hello...world.')
+    
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                insert_user(name)
+                message = f'User {name} successfully added.'
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = f'Error adding {name}: {e}'
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets, message=message)
+
+
+
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset.', users=[])
 
     return app
 
